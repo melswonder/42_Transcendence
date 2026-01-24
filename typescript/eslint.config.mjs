61bef604e3b088b613247aca323adfd9fdf06396
mels wonder <a07054617811@icloud.com>
2026-01-24 15:59:42 +0900

    feat/lintの追加

--- typescript/eslint.config.mjs
+++ typescript/eslint.config.mjs
@@ -1,13 +1,22 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import fsdPlugin from "@conarti/eslint-plugin-feature-sliced/dist/index.js";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    plugins: {
+      "feature-sliced": fsdPlugin,
+    },
+    rules: {
+      "feature-sliced/absolute-public-access": "error",
+      "feature-sliced/layers-slices": "error",
+      "feature-sliced/public-api": "error",
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
